757825231af4b94f0c0a69b0ed85eb14d2d8fe86
hr-ishii <93892269+hr-ishii@users.noreply.github.com>
2022-10-18 16:20:08 +0900

    Update versions.tf
    Organization name change

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "nec-hc-support"
 
     workspaces {
       name = "sentinel-example"
